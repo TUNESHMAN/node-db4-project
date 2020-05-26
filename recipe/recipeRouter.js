52c3f98666c781239a04776052c4f7fa79133a1a
Babatunde <babatundea15@gmail.com>
2020-05-26 14:11:45 +0100

    I fixed endpoint to get ingredients in a recipe

--- recipe/recipeRouter.js
+++ recipe/recipeRouter.js
@@ -16,5 +16,21 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id/shoppingList", (req, res) => {
+  const { id } = req.params;
+  recipe
+    .getShoppingList(id)
+    .then((meal) => {
+      if (meal) {
+        res.status(200).json(meal);
+      } else {
+        res.status(404).json({ message: `error getting list for this recipe` });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message, stack: error.stack });
+    });
+});
+
 // export the router
 module.exports = router;
